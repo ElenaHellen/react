0e4ae4b78a722ebd8225d38d6c75570aff25dd28
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-03 13:17:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import RegisterContainer from '../src/containers';
-import './App.css';     
+//import Counter from '../src/containers';
+import React from 'react'
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <RegisterContainer/>
-    </div>
-  );
+    return ( <div className = "App" >
+
+        <RegisterContainer />
+    
+        {/*<Counter />*/}
+
+        </div>
+    );
 }
 
 export default App;
