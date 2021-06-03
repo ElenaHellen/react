d17444f18892fbfd1b325df70a3e571ed52971f5
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-03 13:15:46 +0200

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,19 +6,11 @@ class RegisterContainer extends React.Component {
         super(props);
         this.state = {
             currentValue: 0,
-            displayForm: false,
-            count: 0,
+           
         };
-        this.handlerAddCounter = this.handlerAddCounter.bind(this)
         this.handleIncrement = this.handleIncrement.bind(this)
         this.handleDecrement = this.handleDecrement.bind(this)
 
-        this.changeDisplayFormStatus = this.changeDisplayFormStatus.bind(this)
-
-    }
-
-    handlerAddCounter = () => {
-        this.setState(prevstate => ({ count: prevstate.count +1}));
     }
 
     handleIncrement() {
@@ -33,23 +25,11 @@ class RegisterContainer extends React.Component {
         });
     }
 
-    changeDisplayFormStatus() {
-
-        this.setState({
-            displayForm: !this.state.displayForm,
-        });
-    }
-
     render() {
-        return ( <
-            Register handlerAddCounter = { this.handlerAddCounter }
-            count = { this.state.count }
-            handleIncrement = { this.handleIncrement }
+        return ( <Register
+                handleIncrement = { this.handleIncrement }
             handleDecrement = { this.handleDecrement }
-            currentValue = { this.state.currentValue }
-
-            displayForm = { this.state.displayForm }
-            changeDisplayFormStatus = { this.changeDisplayFormStatus }
+            currentValue = { this.state.currentValue }          
             />
         );
 
