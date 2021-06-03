a8af827f1d323662e3fde5deb0b7d973947a9a11
Elena <78875237+ElenaHellen@users.noreply.github.com>
2021-06-03 13:16:50 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,33 +1,20 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
 
 const Register = ({
-      handleIncrement, 
-      handleDecrement, 
-      handleIncrementCounter,
-      changeDisplayFormStatus, 
-      currentValue, 
-      displayForm,
-      displayCounter
-}) =>  (
+    handleIncrement,
+    handleDecrement,
+    currentValue,
+   
 
-<div>
-      <div>{currentValue}</div>
-      <button onClick = {handleIncrement}>{currentValue}</button>
-      {displayCounter && 
-      <button onClick = {handleIncrementCounter}>{displayCounter}</button>}
-      <button onClick = {handleDecrement}>-</button>
+}) => (
 
+    <div >
+   
+    <button onClick = { handleIncrement } > { currentValue } </button>   
+    <button onClick = { handleDecrement } > - </button>
 
-      <button onClick={changeDisplayFormStatus}>Change Form Status</button>
-      {displayForm &&
-      <form>
-      <TextField label="Standart" />
-      <TextField label="Filled" />
-      <TextField label="Outlined" />
-      </form>
-      }
-</div>
+    </div>
+
 );
 
 export default Register;
